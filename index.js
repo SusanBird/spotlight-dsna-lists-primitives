904215cc14e4c916d904537dc25a9c378039d1be
SusanBird <susanbird391@gmail.com>
2022-07-12 10:35:18 -0700

    reverseWords function written and passes

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function reverseWords(sentence) {}
+function reverseWords(sentence) {
+    //split sentence into an array of words
+    const words = sentence.split(' ');
+   
+    //map through the words and split each word into an array of letters, keeping the words (arrays) in order
+    // reverse the order of each array
+    const wordArrays = words.map((word) => word.split('').reverse());
+    // console.log(wordArrays);
+
+    // join the letters and words back into a sentence
+    const backwardsWords = wordArrays.map((wordArray) => wordArray.join(''));
+    // console.log(backwardsWords);
+
+    const backwardsSentence = backwardsWords.join(' ');
+    console.log(backwardsSentence);
+    //return the sentence with reversed words
+
+    return backwardsSentence;
+}
 
 module.exports = { reverseWords };
