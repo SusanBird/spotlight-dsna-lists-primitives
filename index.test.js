fc297102637dfbd25e573e2c4cbe1ba6e3cadad4
SusanBird <susanbird391@gmail.com>
2022-07-12 10:37:19 -0700

    title case test written

--- index.test.js
+++ index.test.js
@@ -1,7 +1,14 @@
-const { reverseWords } = require(".");
+const { reverseWords, titleCase } = require(".");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
   });
 });
+
+describe("title case words", () => {
+  it("should capitalize the first letter of each word only", () => {
+    expect(titleCase("alchemy rocks gold")).toBe("Alchemy Rocks Gold");
+  });
+});
+
